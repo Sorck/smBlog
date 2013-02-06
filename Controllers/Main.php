a3a42ca2ebdd0a04467d1f8a7314bcfb9a090159
Sorck <colonel.sorck@gmail.com>
2013-02-06 18:25:06 -0500

    Initial Class skeleton.

---
+++ Controllers/Main.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * smBlog Module - Main Controller
+ *
+ * @package smBlog
+ * @author James Robson
+ * @license MPL 1.1
+ * @version 1.0 Alpha
+ *
+ * The contents of this file are subject to the Mozilla Public License Version 1.1
+ * (the "License"); you may not use this package except in compliance with the
+ * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+ *
+ * The Original Code is smBlog.
+ *
+ * The Initial Developer of the Original Code is James Robson.
+ *
+ * Portions created by the Initial Developer are Copyright (C) 2013
+ * the Initial Developer. All Rights Reserved.
+ * 
+ * @todo Fill in functions
+ * @todo Are tags and search similar in how they would be accessed?
+ */
+
+namespace smCore\Modules\smBlog\Controllers;
+
+use smCore\Module\Controller;
+
+class Main extends Controller
+{
+    public function BlogIndex()
+    {
+        
+    }
+    public function BlogPost()
+    {
+        
+    }
+    public function BlogTags()
+    {
+        
+    }
+    public function BlogSearch()
+    {
+        
+    }
+}
